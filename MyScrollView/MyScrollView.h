3ef0aff8ed812cc2797b15a2b77d3dffdc4d95a1
iantsai <iantsai86@gmail.com>
2015-05-19 15:52:38 -0700

    first commit

---
+++ MyScrollView/MyScrollView.h
@@ -0,0 +1,13 @@
+//
+//  MyScrollView.h
+//  MyScrollView
+//
+//  Created by Ian Tsai on 2015-05-19.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyScrollView : UIView
+
+@end
---
+++ MyScrollView/MyScrollView.m
@@ -0,0 +1,21 @@
+//
+//  MyScrollView.m
+//  MyScrollView
+//
+//  Created by Ian Tsai on 2015-05-19.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import "MyScrollView.h"
+
+@implementation MyScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
